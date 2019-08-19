8240fd468a716e88824c97bc325d7ecdeb1bd102
Rodrigo Robles <rrobles.rodrigo@gmail.com>
2019-08-19 18:32:36 -0400

    adding movie omdb function

--- liri.js
+++ liri.js
@@ -21,14 +21,14 @@ let title = input[3];
 //switch function to determine what the program will do with the first argument the user.
 switch(action){
     case "spotify-this-song":
-        spot(title)
+        spot(title) //call function with spotify API
     break;  
 
     case "movie-this":
-        Movies();
+        movies(title); //call function with movie API and axios
     break;
 
-    case "do-what it says":
+    case "do-what-it-says":
         Read();
     break;
     
@@ -37,19 +37,61 @@ switch(action){
     break;    
 }
 
-//function Spotify to acces the Spotify API
+//function to acces the Spotify API
 function spot(song){
-    spotify.search({ type: "track", query: song }).then(
+    spotify.search({ type: "track", query: song }).then( // search by "track" user input
     function(response) {
-        console.log(response.tracks.items[0].artists[0].name); // Get Artist
-        console.log(response.tracks.items[0].name); // Get Song Name
-        console.log(response.tracks.items[0].album.name); // Get Album Name
-        console.log(response.tracks.items[0].album.external_urls.spotify); // Get Link for song
-
+        console.log(`
+    -------------   
+    Artist: ${response.tracks.items[0].artists[0].name} 
+    Song: ${response.tracks.items[0].name} 
+    Album: ${response.tracks.items[0].album.name}
+    Spotify Link: ${response.tracks.items[0].external_urls.spotify}
+    -------------
+        `); // print artist name, song, album and the link to spotify
 
   })
   .catch(function(err) {
     console.log(err);
+  });
+}
+
+// function 
+function movies(title){
+    const ombdLink = "http://www.omdbapi.com/?t="+ title +"&y=&plot=short&apikey=trilogy"
+
+    axios.get(ombdLink).then(
+  function(response) {
+    console.log(`
+    -------------   
+    Title: ${response.data.Title} 
+    Release Date: ${response.data.Year} 
+    IMBD Rating: ${response.data.imdbRating}
+    Country: ${response.data.Country}
+    Plot: ${response.data.Plot}
+    Actors: ${response.data.Actors}
+    -------------
+        `); // print movie title, year, imdb rating, country of production, plot and actors
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
   });
 }
 
